7fc05468d38463e1b569fd03e85d9edac93d7f0c
sdevine188 <sdevine188@gmail.com>
2016-11-30 13:36:41 -0500

    updated script after ft changed html

--- ft_scraper.R
+++ ft_scraper.R
@@ -29,23 +29,11 @@ get_article_text <- function(all_section_urls) {
         # loop through all sections getting article links for today
         for(section_url in 1:length(all_section_urls)) {
                 current_section_title <- section_titles[section_url]
-                print(str_c(current_section_title, " section"))
                 html <- read_html(all_section_urls[section_url])
-                article_links <- html %>% html_nodes(xpath = "//a[@class = 'card__title']") %>% html_attr("href")
-                article_dates <- html %>% html_nodes(xpath = "//time[@class = 'card__timestamp__time']") %>% html_attr("datetime")
-                article_dates <- lapply(article_dates, function(x) { ymd_hms(x) })
-                article_dates <- do.call(c, article_dates)
-                article_dates_index <- which(day(article_dates) == day(current_date))
-                print(str_c(length(article_dates_index), " articles found"))
-                if(length(article_dates_index) > 0) { 
-                        article_links <- article_links[article_dates_index]
-                } else { next }
+                article_links <- html %>% html_nodes(xpath = "//h2[@class = 'o-teaser__heading']/a") %>% html_attr("href")
                 
                 # build api urls
                 article_urls <- str_c("http://api.ft.com", article_links, "?apiKey=hNIqHozr1JA4f2mU0uDZA2GkZfVzhpQC", sep = "")
-                remove_html_tags <- function(html_string) {
-                        return(str_replace_all(html_string, "<.*?>", ""))
-                }
                 all_article_urls <- c(all_article_urls, article_urls)
                 all_article_sections <- c(all_article_sections, rep(current_section_title, length(article_urls)))
         }
@@ -64,34 +52,44 @@ get_article_text <- function(all_section_urls) {
         all_article_urls <- all_article_urls[content_links_index]
         all_article_sections <- all_article_sections[content_links_index]
         print(str_c("de-duplicated all_article_urls length is ", length(all_article_urls)))
-        
-        # loop through current section getting article text for today
+
+        # loop through article_urls getting article text for today
+        section_tally <- c()
+        article_counter <- 0
+        remove_html_tags <- function(html_string) {
+                return(str_replace_all(html_string, "<.*?>", ""))
+        }
         for(i in 1:length(all_article_urls)) {
                 current_section_title <- all_article_sections[i]
                 article_json <- fromJSON(all_article_urls[i])
-                columnists <- c("Edward Luce", "John Gapper", "Janan Ganesh", "John Thornhill", "Wolfgang Münchau",
-                                "Philip Stephens", "Gillian Tett", "Robert Shrimsley", "Chris Giles", 
-                                "Martin Wolf", "Gideon Rachman", "David Pilling", "David Gardner", "Lawrence Summers")
-                if(current_section_title == "comments" & !(article_json$byline %in% columnists)) {
-                        print("comment author not in columnists")
-                        next
+                article_date <- ymd_hms(article_json$publishedDate)
+                if(day(article_date) == day(current_date)) {
+                        columnists <- c("Edward Luce", "John Gapper", "Janan Ganesh", "John Thornhill", "Wolfgang Münchau",
+                                        "Philip Stephens", "Gillian Tett", "Robert Shrimsley", "Chris Giles", 
+                                        "Martin Wolf", "Gideon Rachman", "David Pilling", "David Gardner", "Lawrence Summers")
+                        if(current_section_title == "comments" & !(article_json$byline %in% columnists)) {
+                                next
+                        }
+                        article_text <- article_json$bodyXML
+                        article_text <- remove_html_tags(article_text)
+                        article_text <- str_c(current_section_title, " section.", "title: ", article_json$title, ". article: ", 
+                                              article_text, "{{split}}", sep = " ")
+                        article_counter <- article_counter + 1
+                        print(article_counter)
+                        all_section_text <- c(all_section_text, article_text)
+                        section_tally <- c(section_tally, current_section_title)
                 }
-                article_text <- article_json$bodyXML
-                article_text <- remove_html_tags(article_text)
-                article_text <- str_c(current_section_title, " section.", "title: ", article_json$title, ". article: ", 
-                                      article_text, "{{split}}", sep = " ")
-                all_section_text <- c(all_section_text, article_text)
         }
 
         # final output
-        print(str_c("complete: ", length(all_section_text), " unique articles"))
+        print(str_c("complete: ", length(all_section_text), " unique articles for ", current_date))
+        section_tally <- data.frame(section = section_tally)
+        section_tally_output <- section_tally %>% group_by(section) %>% tally()
+        print(section_tally_output)
         
         writeLines(all_section_text, "ft.txt")
         print("ft.txt file created")
 }
 
 get_article_text(all_section_urls)
-
-
-
 
